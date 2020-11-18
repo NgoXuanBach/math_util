ef74dd6af114e47e7426d138f954f3ded592ade4
Ngo Xuan Bach <kenhbeyeu3@gmail.com>
2020-11-18 13:06:28 +0700

    add README.md

--- src/math/utlis/MathUtil.java
+++ src/math/utlis/MathUtil.java
@@ -5,7 +5,9 @@
  */
 package math.utlis;
 
-import bach.utils.MathUtility;
+import static bach.utils.MathUtility.*;
+//Câu lệnh từ jdk 8(5) ,khai báo sẵn hàm static
+//ở dưới gọi hàm static 
 
 /**
  *
@@ -18,18 +20,21 @@ public class MathUtil {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        // ta test thử hàm của ta 
+        // ta test thử hàm của ta K CẦN CHẤM 
         //dùng mắt để so sánh kết quả thực tế khi chạy và kết quả ta mong đợi 
         long expected = 120;//ta mong nhận được kết quả 120 nếu gọi hàm 5
-        long actual = MathUtility.getFactorial(5);
+        long actual = getFactorial(5);
         System.out.println("5!:Expected: " + expected + ";Actual" + actual);
         //expected: 720 if tính 6!
-        System.out.println("6!:"+MathUtility.getFactorial(6));
+        System.out.println("6!:" + getFactorial(6));
         //expected: 1 if tính 0!
-        System.out.println("0!:"+MathUtility.getFactorial(0));
+        System.out.println("0!:" + getFactorial(0));
 //expectd: IllegalArgumentException if tính -5;
 
-      System.out.println("-5!:"+MathUtility.getFactorial(-5));
+        System.out.println("-5!:" + getFactorial(-5));
+        // in thử PI COI CÓ GIỐNG Math.PI ?
+        System.out.println("PI:" + PI);
+
     }
 
 }
